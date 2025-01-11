12478c17af970f44d34e9c80d67cf873ce6cfc7d
Rahul Nama <rahulnama0807@gmail.com>
2025-01-11 08:17:39 +0530

    Time: 1 ms (24.12%) | Memory: 9.2 MB (25.51%) - LeetSync

---
+++ 728-self-dividing-numbers/self-dividing-numbers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+int counter(int n){
+    int count = 0;
+    while(n){
+        count++;
+        n /= 10;
+    }
+    return count;
+}
+vector<int> selfDividingNumbers(int left, int right) {
+        vector<int>new_arr;
+        int digit,count = 0, count1 = 0;
+        for(int i = left; i<= right; i++){
+            int temp = i;
+            count = counter(i);
+            count1 = 0;
+            while(temp){
+                digit = temp % 10;
+                if(digit == 0) {
+                    count1 = -1;
+                    break;
+                }
+                if(i % digit == 0){
+                    count1++;
+                }
+                temp /= 10;
+            }
+            if(count == count1){
+                new_arr.push_back(i);
+            }
+        }
+        return new_arr;
+    }
+};
