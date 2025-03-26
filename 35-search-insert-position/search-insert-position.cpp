2a58ebdb7e4082ebb912e14572e3d2178cf6051f
Rahul Nama <rahulnama0807@gmail.com>
2025-03-26 07:39:05 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (42.01%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int binarysearch(vector<int>nums,int start,int end,int target){
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] < target){
+                start = mid + 1;
+            }
+            else if(nums[mid] >= target){
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+    int searchInsert(vector<int>& nums, int target) {
+        return binarysearch(nums,0,nums.size()-1,target);
+    }
+};
