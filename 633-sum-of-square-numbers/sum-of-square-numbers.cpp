87be743c55793af238fbb031de01642199f424d2
Rahul Nama <rahulnama0807@gmail.com>
2025-03-31 20:57:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (20.33%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long a = 0;
+        long b = (long)sqrt(c);
+        while (a <= b) {
+            long t = a * a + b * b;
+            if (t == c) return true;
+            else if (t < c) a++;
+            else b--;
+        }
+        return false;
+    }
+};
